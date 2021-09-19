ab4bff767e585676007a5fe1508d4a11331b4c05
slawomirbuczek <slawekbuczek210@gmail.com>
2021-09-19 15:13:00 +0200

    - init

---
+++ src/main/java/com/edorm/Application.java
@@ -0,0 +1,20 @@
+package com.edorm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Application {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
